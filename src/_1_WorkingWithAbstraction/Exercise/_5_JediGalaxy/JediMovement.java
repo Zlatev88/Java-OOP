836890b91c5a95a1768a4f3791ae5d7575f4c5ca
Zlatin Zlatev <z.zlatev88@gmail.com>
2023-02-01 22:45:16 +0200

    OOP

---
+++ src/_1_WorkingWithAbstraction/Exercise/_5_JediGalaxy/JediMovement.java
@@ -0,0 +1,16 @@
+package _1_WorkingWithAbstraction.Exercise._5_JediGalaxy;
+
+
+public class JediMovement {
+    public long move(int row, int col, Field field){
+        long starsCollected = 0;
+        while (row >= 0 && col < field.getColLength(1)) {
+            if (field.isInBounds(row, col)) {
+                starsCollected += field.getValue(row, col);
+            }
+            col++;
+            row--;
+        }
+        return starsCollected;
+    }
+}
